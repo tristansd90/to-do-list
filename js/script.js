2ad4397cf5ad2edf4f031ad88179297a0b112b52
Tristan SCHMID <tristan.90@icloud.com>
2022-02-13 21:11:02 +0100

    working on local Storage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 const list = document.querySelector("#list");
 const input = document.querySelector("#input");
 const li = document.querySelector("li");
+
 input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         const newLi = document.createElement("li");
         newLi.innerText = input.value;
         list.appendChild(newLi);
+        storage();
         input.value = "";
     }
 });
+
 list.addEventListener("click", (e) => {
     if (e.target.classList.contains("done")) {
         e.target.classList.remove("done");    
     } else {
         e.target.classList.add("done");      
     }
+    storage();
 });
+
 list.addEventListener("dblclick", (e) => e.target.remove());
+
+function storage() {
+    window.localStorage.todolist = list.innerHTML;
+}
+
+
 
 // ----- BACKUP
 // const list = document.querySelector("#list");
