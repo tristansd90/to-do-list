07604dc8fc52772e1be8834a74ca8701dd9a75a2
Tristan SCHMID <tristan.90@icloud.com>
2022-02-15 17:49:51 +0100

    add toggle on js

--- js/script.js
+++ js/script.js
@@ -13,11 +13,12 @@ input.addEventListener("keydown", (e) => {
 });
 
 list.addEventListener("click", (e) => {
-    if (e.target.classList.contains("done")) {
-        e.target.classList.remove("done");    
-    } else {
-        e.target.classList.add("done");      
-    }
+    e.target.classList.toggle("done");
+    // if (e.target.classList.contains("done")) {
+    //     e.target.classList.remove("done");    
+    // } else {
+    //     e.target.classList.add("done");      
+    // }
     storage();
 });
 
@@ -25,32 +26,4 @@ list.addEventListener("dblclick", (e) => e.target.remove());
 
 function storage() {
     window.localStorage.todolist = list.innerHTML;
-}
-
-
-
-// ----- BACKUP
-// const list = document.querySelector("#list");
-// const input = document.querySelector("#input");
-// const li = document.querySelector("li");
-
-// input.addEventListener("keydown", (e) => {
-//     if (e.key === "Enter") {
-//         const newLi = document.createElement("li");
-//         newLi.innerText = input.value;
-//         list.appendChild(newLi);
-//         input.value = "";
-//     }
-// });
-
-// list.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("done")) {
-//         e.target.classList.remove("done");    
-//     } else {
-//         e.target.classList.add("done");      
-//     }
-// });
-
-// list.addEventListener("dblclick", (e) => {
-//     e.target.remove();
-// });
+};
